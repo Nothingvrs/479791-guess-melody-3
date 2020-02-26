631b688385a70916cde2cada6a3606caff3d54b4
b00m_ <Rmn205Vrs39>
2020-02-26 14:01:36 +0300

    Добавил Redux. Вынес состояние приложения в Redux. Добавил проверку ответов на вопросы.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,48 +1,34 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
+import {connect} from "react-redux";
+import {ActionCreator} from "../../reducer.js";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
+import GameScreen from "../game-screen/game-screen.jsx";
 import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
 import {GameType} from "../../const.js";
-import GameScreen from "../game-screen/game-screen.jsx";
 import withAudioPlayer from "../../hocs/with-audio-player/with-audio-player.js";
-
 
 const GenreQuestionScreenWrapped = withAudioPlayer(GenreQuestionScreen);
 const ArtistQuestionScreenWrapped = withAudioPlayer(ArtistQuestionScreen);
 
 class App extends PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      step: -1,
-    };
-  }
-
   _renderGameScreen() {
-    const {errorsCount, questions} = this.props;
-    const {step} = this.state;
+    const {
+      maxMistakes,
+      questions,
+      onUserAnswer,
+      onWelcomeButtonClick,
+      step,
+    } = this.props;
     const question = questions[step];
-
-    const setWelcomeScreen = () => {
-      this.setState({
-        step: 0,
-      });
-    };
-
-    const setNewQuestion = () => {
-      this.setState((prevState) => ({
-        step: prevState.step + 1,
-      }));
-    };
 
     if (step === -1 || step >= questions.length) {
       return (
         <WelcomeScreen
-          errorsCount={errorsCount}
-          onWelcomeButtonClick={setWelcomeScreen}
+          errorsCount={maxMistakes}
+          onWelcomeButtonClick={onWelcomeButtonClick}
         />
       );
     }
@@ -50,21 +36,30 @@ class App extends PureComponent {
     if (question) {
       switch (question.type) {
         case GameType.ARTIST:
-          return (<GameScreen type={question.type}>
-            <ArtistQuestionScreenWrapped
-              question={question}
-              onAnswer={setNewQuestion}
-            /></GameScreen>
+          return (
+            <GameScreen
+              type={question.type}
+            >
+              <ArtistQuestionScreenWrapped
+                question={question}
+                onAnswer={onUserAnswer}
+              />
+            </GameScreen>
           );
         case GameType.GENRE:
-          return (<GameScreen type={question.type}>
-            <GenreQuestionScreenWrapped
-              question={question}
-              onAnswer={setNewQuestion}
-            /></GameScreen>
+          return (
+            <GameScreen
+              type={question.type}
+            >
+              <GenreQuestionScreenWrapped
+                question={question}
+                onAnswer={onUserAnswer}
+              />
+            </GameScreen>
           );
       }
     }
+
     return null;
   }
 
@@ -96,8 +91,29 @@ class App extends PureComponent {
 }
 
 App.propTypes = {
-  errorsCount: PropTypes.number.isRequired,
+  maxMistakes: PropTypes.number.isRequired,
   questions: PropTypes.array.isRequired,
+  onUserAnswer: PropTypes.func.isRequired,
+  onWelcomeButtonClick: PropTypes.func.isRequired,
+  step: PropTypes.number.isRequired,
 };
 
-export default App;
+const mapStateToProps = (state) => ({
+  step: state.step,
+  maxMistakes: state.maxMistakes,
+  questions: state.questions,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onWelcomeButtonClick() {
+    dispatch(ActionCreator.incrementStep());
+  },
+  onUserAnswer(question, answer) {
+    dispatch(ActionCreator.incrementMistake(question, answer));
+    dispatch(ActionCreator.incrementStep());
+  },
+});
+
+
+export {App};
+export default connect(mapStateToProps, mapDispatchToProps)(App);
